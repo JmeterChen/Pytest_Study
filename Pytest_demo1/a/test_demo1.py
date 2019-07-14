6ccbc2b10a94898d5d026892d24d61e5b6fa8017
chenwenjian <chenwenjian@fangdd.com>
2019-07-14 11:29:29 +0800

    fix:add tgz of task

--- Pytest_demo1/a/test_demo1.py
+++ Pytest_demo1/a/test_demo1.py
@@ -4,14 +4,32 @@
 
 import  pytest
 
+a = 100
 
-def test_case03():
+
+def test_case01():
+    global a
+    a += 1
+    print(a)
+    assert a >= 101
+
+
+def test_case02():
     print(3)
 
 
-def test_huigui02():
-    print('huigui02')
+def test_case03():
+    assert (1, 2, 3) == (3, 2, 1)
+    
 
+def test_case04():
+    print('2+3 <= 4')
+    assert 2 + 3 <= 4
+    
+    
+def test_case05():
+    print('case06')
+    assert 'kobe' not in ['kobe', 'james', 'Wade']
 
 if __name__ == '__main__':
     pytest.main()
--- Pytest_demo1/test_demo.py
+++ Pytest_demo1/test_demo.py
@@ -3,37 +3,44 @@
 # Datetime: 2019/6/15 21:05
 
 import pytest
+import time
+
 
 class Test_demo():
 
-    def test_case01(self):
+    def test_case06(self):
         print(1)
 
-    def test_case02(self):
+    def test_case07(self):
         print(2)
 
 
 class Test_demo2():
 
-    def test_case03(self):
+    def test_case08(self):
         print(3)
 
 
 @pytest.mark.huigui
 @pytest.mark.smoke
-def test_case04():
+def test_case09():
     print(3)
 
 
-def test_case05():
+def test_case10():
+    assert 5 <= 4
     print(5)
 
 
 @pytest.mark.smoke
-def test_case06():
+def test_case11():
     print(6)
 
 
 @pytest.mark.huigui
-def test_huigui01():
+def test_case12():
     print('huigui01')
+    
+    
+def test_case13():
+    time.sleep(0.1)
